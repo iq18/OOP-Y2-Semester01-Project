c4878e9fdaa9376e922ab044c8cffd2c26912e8b
iq18 <36903198+iq18@users.noreply.github.com>
2018-08-16 15:18:11 +0100

    Restructure test class, added comments.

--- src/main/java/app/Test.java
+++ src/main/java/app/Test.java
@@ -25,30 +25,36 @@ public class Test {
 		// Test Objects
 		Phone p1 = new Phone("Phone", "HD", 230.00, "Samsung", "S7", 128);
 		Phone p2 = new Phone("Phone", "Lightweight", 650.00, "Apple", "X", 128);
+		Phone p3 = new Phone("Phone", "Lightweight", 750.00, "Apple", "X", 128);
 
 		TV t1 = new TV("TV", "Large screen", 559.00, "Toshiba", 55, TVType.LED, true);
+		
 		// print Objects
 		p1.print();
 		p2.print();
 		t1.print();
 
-		Phone p3 = new Phone("Phone", "Lightweight", 750.00, "Apple", "X", 128);
-
+		//DB Object to hold list of products
 		ProductDB db = new ProductDB();
+		
+		//Test functionality
 		db.addProduct(p1);
 		db.addProduct(p2);
 		db.addProduct(t1);
 		db.addProduct(p3);
 
-		/*
-		 * for(Product p : db.getProductList()) { System.out.println(p.getName() + " " +
-		 * p.getProductID()); }
-		 */
 		db.removeProduct(p2);
 		Product a = db.findProduct(p2);
 		if(a != null) {
 			System.out.println("Product found");
 		}
+		
+		
+		
+		/*
+		 * for(Product p : db.getProductList()) { System.out.println(p.getName() + " " +
+		 * p.getProductID()); }
+		 */
 		
 	}
 
